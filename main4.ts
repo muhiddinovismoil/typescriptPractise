297f802a50301c3968124ce6559958048200059d
Ismoil <muhiddinovismoil2@gmail.com>
2024-12-07 20:47:02 +0500

    Updating main4.ts file

--- main4.ts
+++ main4.ts
@@ -1,4 +1,4 @@
-type Connection = {
+interface Connection {
     client: string;
     connection: {
         user: string;
@@ -6,26 +6,24 @@ type Connection = {
         port: number;
         database: string;
     };
-};
+}
 class Database<T, T2> {
     private connection: T[] = [];
-    private data: T2 = [];
+    private data: T2[] = [];
     connect(boot: T): void {
-        console.log(`You are now connected to Database ${boot.client}`);
+        this.connection.push(boot);
+        console.log(`You are now connected to Database`);
     }
     create(items: T2): void {
         this.data.push(items);
         console.log(`Data saved to your database`);
     }
-    read(): void {
+    read(): T2[] {
         if (this.data.length == 0) {
             console.error(`Date not found`);
-            return [];
+            return;
         }
-        console.log({
-            msg: "All Data",
-            AllData: this.data,
-        });
+        return this.data;
     }
     update(id: number, updatedData: T2): void {
         const index = this.data.findIndex((item: any) => item.id === id);
@@ -38,20 +36,68 @@ class Database<T, T2> {
             ...updatedData,
         };
     }
-    delete(): void {
+    delete(id: number): void {
         const index = this.data.findIndex((item: any) => item.id === id);
         if (index == -1) {
             console.error(`Data not found or maybe deleted recently`);
             return;
         }
+        this.data.splice(index, 1);
+        console.log(`Data deleted with id: ${id}`);
+    }
+    disconnect(): void {
+        console.log(`Database disconnected`);
+        this.connection.length = 0;
     }
 }
-class MongoDB extends Database<Connection, T2> {
-    constructor() {}
+class MongoDB<T2> extends Database<Connection, T2> {
+    constructor() {
+        super();
+    }
 }
-class SQLDB extends Database<Connection, T2> {
-    constructor() {}
+class SQLDB<T2> extends Database<Connection, T2> {
+    constructor() {
+        super();
+    }
 }
-let mngdb = new MongoDB<{ name: string; age: number }>();
+const mongoConnection: Connection = {
+    client: "MongoDB",
+    connection: {
+        user: "admin",
+        host: "localhost",
+        port: 27017,
+        database: "testDb",
+    },
+};
 
-let sql = new SQLDB<{ name: string; age: number }>();
+let mngdb = new MongoDB<{ id?: number; name: string; age: number }>();
+// MONGO DB
+mngdb.connect(mongoConnection);
+mngdb.create({ id: 1, name: "Ali", age: 25 });
+mngdb.create({ id: 2, name: "Vali", age: 30 });
+console.log("MongoDB Data:", mngdb.read());
+mngdb.update(1, { name: "Alisher", age: 26 });
+console.log("Updated MongoDB Data:", mngdb.read());
+mngdb.delete(1);
+console.log("MongoDB Data after deletion:", mngdb.read());
+mngdb.disconnect();
+
+// let sql = new SQLDB<{ id: number; name: string; age: number }>();
+// const sqlConnection: Connection = {
+//     client: "SQLDB",
+//     connection: {
+//         user: "root",
+//         host: "localhost",
+//         port: 3306,
+//         database: "exampleDb",
+//     },
+// };
+// // SQLDB example
+// sql.connect(sqlConnection);
+// sql.create({ id: 1, name: "Hasan", age: 40 });
+// sql.create({ id: 2, name: "Husan", age: 35 });
+// console.log("SQLDB Data:", sql.read());
+// sql.update(2, { name: "Husanbek", age: 36 });
+// console.log("Updated SQLDB Data:", sql.read());
+// sql.delete(2);
+// console.log("SQLDB Data after deletion:", sql.read());
